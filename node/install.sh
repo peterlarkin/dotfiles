3efbc012f82fea90d5dfa5ad8ced183f0a2e3848
Peter Larkin <prlarkin@me.com>
2017-06-16 22:21:32 +0100

    Add note about getting n working

--- node/install.sh
+++ node/install.sh
@@ -1,8 +1,11 @@
 echo '> installing global npm packages'
 
-npm install n -g
 npm install grunt-cli -g
 npm install gulp -g
 npm install bower -g
 npm install yo -g
 npm install eslint -g
+
+# Currently to get n working, you need to run
+# brew uninstall node --ignore-dependencies
+npm install n -g
