4911703081057727561c38a71d91f53e3f6f9111
Peter Larkin <prlarkin@me.com>
2024-10-01 23:06:20 +0100

    Simplify node global list

--- node/install.sh
+++ node/install.sh
@@ -1,11 +1,4 @@
 echo '> installing global npm packages'
 
-npm install grunt-cli -g
 npm install gulp -g
-npm install bower -g
-npm install yo -g
 npm install eslint -g
-
-# Currently to get n working, you need to run
-# brew uninstall node --ignore-dependencies
-npm install n -g
